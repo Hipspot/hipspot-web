c5a588318926522d1578f38bd47dad965ea4d94d
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-01-08 21:50:53 +0900

    Chore: eslint, babel 설정, test환경 설정

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,26 @@
 const path = require('path');
-const { useBabelRc, addWebpackAlias, override } = require('customize-cra');
+const { getBabelLoader, addWebpackAlias, override } = require('customize-cra');
 
 module.exports = override(
-	useBabelRc(),
+	removeBuiltinBabelConfig,
+	enableBabelConfig,
 	addWebpackAlias({
 		'@components': path.resolve(__dirname, 'src', 'components'),
 		'@recoil': path.resolve(__dirname, 'src', 'recoil'),
 	})
 );
+
+function removeBuiltinBabelConfig(config) {
+	const loader = getBabelLoader(config);
+
+	loader.options.presets = [];
+	loader.options.plugins = [];
+
+	return config;
+}
+
+function enableBabelConfig(config) {
+	const loader = getBabelLoader(config);
+	loader.options.configFile = path.resolve(__dirname, 'babel.config.js');
+	return config;
+}
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 test('renders learn react link', () => {
-	render(<App />);
+	render(
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
+	);
 	const linkElement = screen.getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
