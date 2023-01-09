7a5b7e49048bb518ea4a61f94cc6b28e05c9ceb5
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-01-09 18:03:54 +0900

    Feat: 필터 아이콘 SVG 추가

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,14 @@
+// Filtering 리스트의 아이콘 Gray Scale과 Colored 된 아이콘 각각 존재함
+export { ReactComponent as HipspotGray } from './HipspotGray.svg';
+export { ReactComponent as StudyGray } from './StudyGray.svg';
+export { ReactComponent as EffectiveGray } from './EffectiveGray.svg';
+export { ReactComponent as DessertGray } from './DessertGray.svg';
+export { ReactComponent as FranchiseGray } from './FranchiseGray.svg';
+export { ReactComponent as IndividualGray } from './IndividualGray.svg';
+
+export { ReactComponent as HipspotColored } from './HipspotColored.svg';
+export { ReactComponent as StudyColored } from './StudyColored.svg';
+export { ReactComponent as EffectiveColored } from './EffectiveColored.svg';
+export { ReactComponent as DessertColored } from './DessertColored.svg';
+export { ReactComponent as FranchiseColored } from './FranchiseColored.svg';
+export { ReactComponent as IndividualColored } from './IndividualColored.svg';
