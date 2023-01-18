db0e01e8c32f391c1c6846fdcab52036e66e4d91
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-01-18 19:28:41 +0900

    Feat: getPlace API Mocking #6

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
-import './index.css';
+import axios from 'axios';
 import App from './App';
+import { worker } from './mocks';
+import './index.css';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+} else {
+  axios.defaults.baseURL = '';
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<RecoilRoot>
-		<App />
-	</RecoilRoot>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
