cad75e9375aeda221d03b45d7f0354dc2b3193b9
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-01-24 18:21:53 +0900

    Refactor: Path 수정 및 Alias 적용 #24

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@ module.exports = override(
   addWebpackAlias({
     '@assets': path.resolve(__dirname, 'src', 'assets'),
     '@components': path.resolve(__dirname, 'src', 'components'),
+    '@containers': path.resolve(__dirname, 'src', 'containers'),
     '@constants': path.resolve(__dirname, 'src', 'constants'),
     '@libs': path.resolve(__dirname, 'src', 'libs'),
     '@recoil': path.resolve(__dirname, 'src', 'recoil'),
