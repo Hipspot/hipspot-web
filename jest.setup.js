7c6cb9df98ec67a8345b1e8933c67341ef9221a1
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-01-27 20:08:11 +0900

    Feat: InfoWindow 테스트 환경 구축 #26

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,11 @@
-beforeAll(() => {});
+import { server } from '@mocks/server';
 
-afterAll(() => {});
+// Establish API mocking before all tests.
+beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
