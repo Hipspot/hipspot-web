e8070f26b5b21a29149cc04a4af5f0c591df437d
Sangjun-man <molung@icloud.com>
2023-01-30 01:20:09 +0900

    feat : 마커 클릭 시 인포윈도우 팝업 기능 구현

--- src/containers/MapCompContainer.tsx
+++ src/containers/MapCompContainer.tsx
@@ -1,13 +1,15 @@
 import MapComp from '@components/MapComp';
 import { Suspense } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { activatedCafeIdAtom } from '@states/infoWindow';
+import { activatedCafeIdAtom, tabStateAtom } from '@states/infoWindow';
+import { popUpHeights, PopUpHeightsType } from '@constants/popUpHeights';
 
 function MapCompContainer() {
   const setActivatedCafeId = useSetRecoilState(activatedCafeIdAtom);
-
+  const setTabState = useSetRecoilState(tabStateAtom);
   const handleClickMarker = (id: number) => {
     setActivatedCafeId(id);
+    setTabState((prev) => ({ ...prev, top: popUpHeights[PopUpHeightsType.middle] }));
   };
 
   return (
