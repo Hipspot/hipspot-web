b9e0afbef05c1070348d10016d25a73282c856e9
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-02-02 11:33:18 +0900

    Fix: API 엔드포인트 수정 #38

--- src/libs/apis/getGeoJson.ts
+++ src/libs/apis/getGeoJson.ts
@@ -1,15 +1,12 @@
 import axios from 'axios';
 
 const getGeoJson = async () => {
-  const response = await axios({
-    url: `${process.env.REACT_APP_API_URL}/map`,
-    method: 'get',
-  })
-    .then((res) => res.data)
-    .catch((err) => {
-      throw new Error(err);
-    });
-  return response;
+  try {
+    const response = await axios.get('/map');
+    return response.data;
+  } catch (error) {
+    throw Error('Error get getGeoJson');
+  }
 };
 
 export default getGeoJson;
