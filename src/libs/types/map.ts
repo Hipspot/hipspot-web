6178ed22fd4a0de6e86f168d735917bd687b2ad9
yeoularu <yeoularu@gmail.com>
2023-02-07 16:54:04 +0900

    chore: CustomGeoJSONFeatures example 수정

--- src/libs/types/map.ts
+++ src/libs/types/map.ts
@@ -14,18 +14,18 @@ interface GeoJSONPointType {
  * @description mapbox source에 들어갈 GeoJSON 타입
  * 
  * @example
-{
-  "properties": {
-    "instaId": "@cafeknotted",
-    "id": "10",
-    "placeName": "카페 노티드 성수",
-    "filterList": [4, 3]
+ * {
+  properties: {
+    instaId: "@cafeknotted",
+    id: 10,
+    placeName: "카페 노티드 성수",
+    filterList: [4, 3]
   },
-  "geometry": {
-    "type": "Point",
-    "coordinates": [127.051352, 37.5446694]
+  geometry: {
+    type: "Point",
+    coordinates: [127.051352, 37.5446694]
   },
-  "type": "Feature"
+  type: "Feature"
 }
  */
 export interface CustomGeoJSONFeatures {
