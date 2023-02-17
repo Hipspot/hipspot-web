4a659e7ccf4379d4ffc602de42ac5ca637fd2265
Sangjun-man <molung@icloud.com>
2023-02-17 17:33:08 +0900

    refactor : 주석처리 되어있던 selector코드 삭제

--- src/states/clusterList.ts
+++ src/states/clusterList.ts
@@ -16,21 +16,3 @@ export const activeFilterIdAtom = atom<FilterId>({
   key: 'filterId',
   default: FilterId.Hipspot,
 });
-
-// export const clusterListStateSelector = selectorFamily({
-//   key: 'selector / clusteListState',
-//   get:
-//     (leafIds: number[]) =>
-//     ({ get }) => {
-//       if (!leafIds.length) return [];
-
-//       const geoJsonData = get(geoJsonAtom);
-//       const filterByleafId = (leafId: number) =>
-//         geoJsonData.filter(({ properties }) => Number(properties.id) === leafId)[0] || null;
-
-//       return leafIds.map((leafId: number) => filterByleafId(leafId));
-//     },
-
-//   // 파라미터로 전달하는 id Array 값 계속 바뀌니까 캐싱 하지 않음
-//   cachePolicy_UNSTABLE: { eviction: 'most-recent' },
-// });
