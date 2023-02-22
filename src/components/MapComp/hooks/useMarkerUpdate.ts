18c94e958d0e9967dbfcb1f6cff4b4a4a30c2486
yeoularu <yeoularu@gmail.com>
2023-02-23 00:27:11 +0900

    Refactor: updateMarker 함수 useMarkerUpdate 훅으로 포팅

---
+++ src/components/MapComp/hooks/useMarkerUpdate.ts
@@ -0,0 +1,112 @@
+import ClusterMarker from '@components/Marker/clusterMarker';
+import PointMarker from '@components/Marker/pointMarker';
+import { MarkerList } from '@libs/types/map';
+import { renderEmotionElementToHtml } from '@libs/utils/renderEmotionElementToHtml';
+import { activeFilterIdAtom } from '@states/clusterList';
+import { geoJsonAtom } from '@states/map';
+import mapboxgl, { GeoJSONSource, Marker } from 'mapbox-gl';
+import { useRecoilValue } from 'recoil';
+import getFeaturesOnScreen from '../utils/getFeaturesOnScreen';
+import useMapRef from './useMap';
+import useMarkerClickAction from './useMarkerClickAction';
+
+const pointMarkerList: MarkerList = {};
+const clusterMarkerList: MarkerList = {};
+
+function useMarkerUpdate() {
+  const mapRef = useMapRef();
+  const allFeatures = useRecoilValue(geoJsonAtom);
+  const filterId = useRecoilValue(activeFilterIdAtom);
+  const { pointMarkerClickAction, clusterMarkerClickAction } = useMarkerClickAction();
+
+  const updateMarkers: () => void = () => {
+    const map = mapRef.current;
+    if (!map) return;
+    const { pointFeaturesOnScreen, clusterFeaturesOnScreen, uniquePointIds, uniqueClusterIds } = getFeaturesOnScreen({
+      map,
+      allFeatures,
+      filterId,
+    });
+
+    // point markers
+    pointFeaturesOnScreen.forEach((feature) => {
+      const { id } = feature.properties;
+      if (Object.hasOwn(pointMarkerList, id)) return;
+      try {
+        const marker = renderEmotionElementToHtml({
+          elem: PointMarker({
+            handleClickPointMarker: pointMarkerClickAction(filterId),
+            feature,
+            activeFilterId: filterId,
+            image: 'https://hipspotimage.s3.ap-northeast-2.amazonaws.com/bluemilescoffee/store/0.jpg',
+            id,
+          }),
+          cssDataKey: 'marker',
+        });
+        pointMarkerList[id] = new mapboxgl.Marker(marker, { anchor: 'bottom' })
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(map);
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+      }
+    });
+
+    // remove points markers
+    Object.entries(pointMarkerList).forEach((markerEntry) => {
+      const [id, marker] = markerEntry as [string, Marker];
+
+      if (uniquePointIds.has(Number(id))) return;
+      marker.remove();
+      delete pointMarkerList[id];
+    });
+
+    // cluster markers
+    clusterFeaturesOnScreen.forEach((feature) => {
+      const clusterId = feature.properties?.cluster_id;
+      if (Object.hasOwn(clusterMarkerList, clusterId)) return;
+
+      const count = feature.properties?.point_count;
+      const geo = feature.geometry;
+
+      const marker = renderEmotionElementToHtml({
+        elem: ClusterMarker({
+          count,
+          filterId,
+          handleClickClusterMarker: (id: number) => {
+            const source = map.getSource(`cafeList/${filterId}`) as GeoJSONSource;
+
+            source.getClusterLeaves(id, 200, 0, (err, leaves) => {
+              const properties = leaves.map((leaf) => ({
+                ...leaf.properties,
+                // 서버에서 geojson 피쳐에 이미지 담아주면 그 이미지 링크로 수정 필요
+                image: 'https://hipspotimage.s3.ap-northeast-2.amazonaws.com/bluemilescoffee/store/0.jpg',
+              }));
+              clusterMarkerClickAction(properties);
+            });
+          },
+          clusterId,
+        }),
+        cssDataKey: 'cluster',
+      });
+
+      if (geo.type === 'GeometryCollection') return;
+      clusterMarkerList[clusterId] = new mapboxgl.Marker(marker)
+        .setLngLat(geo.coordinates as [number, number])
+        .addTo(map);
+    });
+
+    // remove cluster markers
+    Object.entries(clusterMarkerList).forEach((markerEntry) => {
+      const [id, marker] = markerEntry as [string, Marker];
+
+      if (uniqueClusterIds.has(Number(id))) return;
+      marker.remove();
+      delete clusterMarkerList[id];
+    });
+  };
+
+  return updateMarkers;
+}
+
+export default useMarkerUpdate;
--- src/components/MapComp/index.tsx
+++ src/components/MapComp/index.tsx
@@ -1,67 +1,36 @@
-import React, { useEffect, useMemo, useRef } from 'react';
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { css } from '@emotion/react';
 import { geoJsonAtom } from '@states/map';
 import { activeFilterIdAtom } from '@states/clusterList';
 import { MapboxEvent } from 'mapbox-gl';
-import { MarkerList } from '@libs/types/map';
 import { FindMyLocationEvent } from '@libs/types/customEvents';
 import { EVENT_FIND_MY_LOCATION } from '@constants/event';
 import { DOMID_MAP_COMPONENT } from '@constants/DOM';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import removeAllMarkers from './utils/removeAllMarkers';
 import addFeatureLayerByFilterId from './eventHandler/addFeatureLayerByFilterId';
-import updateMarker from './eventHandler/updateMarker';
 import drawPulsingDotMarker from './eventHandler/drawPulsingDotMarker';
 import { DOMTargetList } from '../../constants/DOM';
 import useCameraMove from './hooks/useCameraMove';
 import useMap from './hooks/useMap';
+import useMarkerUpdate from './hooks/useMarkerUpdate';
 
-type MapCompProps = {
-  pointMarkerClickAction: (filterId: number) => (id: number) => void;
-  clusterMarkerClickAction: (id: any[]) => void;
-};
-
-function MapComp({ pointMarkerClickAction, clusterMarkerClickAction }: MapCompProps) {
+function MapComp() {
   const activeFilterId = useRecoilValue(activeFilterIdAtom);
   const allFeatures = useRecoilValue(geoJsonAtom);
-  const pointMarkerList: MarkerList = useMemo(() => ({}), []);
-  const clusterMarkerList: MarkerList = useMemo(() => ({}), []);
-  const activeFilterIdRef = useRef(activeFilterId);
   const mapRef = useMap();
 
   const { flyTo, savePrevPostion } = useCameraMove();
+  const updateMarkers = useMarkerUpdate();
   const onMapLoad = ({ target: targetMap }: MapboxEvent) => addFeatureLayerByFilterId({ map: targetMap, allFeatures });
-  const onRender = ({ target: targetMap }: MapboxEvent) => {
-    const filterId = activeFilterIdRef.current;
-    updateMarker({
-      map: targetMap,
-      allFeatures,
-      pointMarkerList,
-      clusterMarkerList,
-      clusterMarkerClickAction,
-      pointMarkerClickAction: pointMarkerClickAction(filterId),
-      filterId,
-    });
-  };
+  const onRender = () => updateMarkers();
   const onMoveEnd = ({ target: targetMap }: MapboxEvent) =>
     savePrevPostion(targetMap.getCenter(), { zoom: targetMap.getZoom() });
 
   useEffect(() => {
     const map = mapRef.current;
     if (!map) return;
-    activeFilterIdRef.current = activeFilterId;
-    const filterId = activeFilterIdRef.current;
-    removeAllMarkers({ pointMarkerList, clusterMarkerList });
-    updateMarker({
-      map,
-      allFeatures,
-      pointMarkerList,
-      clusterMarkerList,
-      clusterMarkerClickAction,
-      pointMarkerClickAction: pointMarkerClickAction(filterId),
-      filterId,
-    });
+    updateMarkers();
     map.on('load', onMapLoad);
     map.on('render', onRender);
     map.on('moveend', onMoveEnd);
