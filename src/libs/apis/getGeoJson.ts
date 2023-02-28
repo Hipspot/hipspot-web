680da926a67f6682c5b3e7ec6bc5032d4f41dc53
yeoularu <yeoularu@gmail.com>
2023-02-28 20:28:59 +0900

    fix: map api url 롤백

--- src/libs/apis/getGeoJson.ts
+++ src/libs/apis/getGeoJson.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getGeoJson = async () => {
   try {
-    const response = await axios.get('https://api.hipspot.xyz/map');
+    const response = await axios.get('/map');
     return response.data;
   } catch (error) {
     throw Error('Error get getGeoJson');
