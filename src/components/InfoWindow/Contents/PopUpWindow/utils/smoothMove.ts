4def7659dea53fec09c1c32c41d63f221aa90bcc
yeoularu <yeoularu@gmail.com>
2023-04-16 17:14:56 +0900

    refactor: smoothMove 함수 성능개선

--- src/components/InfoWindow/Contents/PopUpWindow/utils/smoothMove.ts
+++ src/components/InfoWindow/Contents/PopUpWindow/utils/smoothMove.ts
@@ -9,22 +9,21 @@ interface SmoothMoveArgs {
 }
 
 export default function smoothMove({ parentElement, endPointTabState, smoothLoopId }: SmoothMoveArgs) {
-  let debounce = 60;
   let curY = parentElement.getBoundingClientRect().y;
+
   function loop() {
     const endY = endPointTabState.top;
     const acc = 0.1;
     curY += (endY - curY) * acc;
 
     modifyInfoWindowTop({ currentTop: curY });
-    const check = curY - endPointTabState.top > -1 && curY - endPointTabState.top < 1;
-    if (debounce <= 1 || check) {
-      return modifyInfoWindowTop({ currentTop: endPointTabState.top });
+
+    if (Math.abs(curY - endY) > 1) {
+      smoothLoopId.id = requestAnimationFrame(loop);
+    } else {
+      modifyInfoWindowTop({ currentTop: endY });
     }
-    debounce -= 1;
-    smoothLoopId.id = requestAnimationFrame(loop);
-    return smoothLoopId.id;
   }
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  smoothLoopId.id = loop()!;
+
+  smoothLoopId.id = requestAnimationFrame(loop);
 }
