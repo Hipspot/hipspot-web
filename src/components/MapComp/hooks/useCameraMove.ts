3fbaaff5b219bd0b9612e5c2e84fde75b15fa9bd
yeoularu <yeoularu@gmail.com>
2023-04-17 15:17:42 +0900

    fix: tilt 제거 롤백

--- src/components/MapComp/hooks/useCameraMove.ts
+++ src/components/MapComp/hooks/useCameraMove.ts
@@ -70,14 +70,14 @@ function useCameraMove() {
 export default useCameraMove;
 
 const tiltNextState = ({ coordinate }: { coordinate: LngLatLike }): FlyToOptions => {
-  // const RAD = 15;
+  const RAD = 15;
   const DURATION = 500;
-  // const PITCH = 50;
+  const PITCH = 50;
 
   return {
     center: coordinate,
-    // bearing: -2 * RAD - 3,
-    // pitch: PITCH,
+    bearing: -2 * RAD - 3,
+    pitch: PITCH,
     duration: DURATION,
     zoom: 18,
   };
