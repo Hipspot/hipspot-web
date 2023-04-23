67e7764c82b4b4a7e224f5e9abf7421c43b2ebac
hyorish03 <hyorish2001@gmail.com>
2023-04-24 01:20:31 +0900

    카페정보 API 요청

--- src/components/Button/RandomButton/index.tsx
+++ src/components/Button/RandomButton/index.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
 import { RandomButtonIcon } from '@assets/index';
-import { toast } from 'react-hot-toast';
 import { useRecoilValue } from 'recoil';
 import { tabStateAtom } from '@states/infoWindow';
+import axios from 'axios';
 
 function RandomButton() {
   const { top } = useRecoilValue(tabStateAtom);
+  const [cafeIds, setCafeIds] = useState([] as any);
 
-  const onClick = () => {
-    toast.success('click random button');
+  const fetchCafeId = async () => {
+    try {
+      setCafeIds(null);
+      const response = await axios.get('https://api.hipspot.xyz/cafe/:cafeId');
+      setCafeIds(response.data.cafeId);
+    } catch (e) {
+      throw Error('Error throwed');
+    }
   };
+  useEffect(() => {
+    fetchCafeId();
+  }, []);
 
+  if (!cafeIds) return null;
+
+  cafeIds.map((cafeId: { id: number }) => <li key={cafeIds.id}>{cafeId.id}</li>);
+
+  const onClick = () => {};
   return (
     <S.Wrapper onClick={onClick} top={top}>
       <RandomButtonIcon />
     </S.Wrapper>
   );
 }
-
 export default RandomButton;
