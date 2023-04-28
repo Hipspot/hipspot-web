41e1f58bed846b54f45b951611a733f70844e77e
Sangjun-man <molung@icloud.com>
2023-04-28 17:14:59 +0900

    fix: timeGap 수정

--- src/constants/interaction.ts
+++ src/constants/interaction.ts
@@ -1,4 +1,4 @@
-export const FLICKING_TIMEGAP = 80;
+export const FLICKING_TIMEGAP = 100;
 export const FLICKING_DISTANCE = 40;
 export const HORIZONTAL_MOVE_DISTANCE = 20;
 export const LONGPRESS_TIMEGAP = 150;
