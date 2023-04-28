a254dd085b86644f2aa91da69ebefdd2b2b81e34
Sangjun-man <molung@icloud.com>
2023-04-28 14:42:23 +0900

    fix: popUpHeight 잘못 기입된것 수정

--- src/constants/popUpHeights.ts
+++ src/constants/popUpHeights.ts
@@ -10,7 +10,7 @@ export const popUpHeights = {
   [PopUpHeightsType.top]: -BarSizes[BarSizeType.BOTTOM],
   [PopUpHeightsType.middle]: window.innerHeight * 0.6,
   [PopUpHeightsType.bottom]: window.innerHeight - 30,
-  invisible: -BarSizes[BarSizeType.BOTTOM],
+  invisible: window.innerHeight - 30,
   half: window.innerHeight * 0.6,
-  full: window.innerHeight - 30,
+  full: -BarSizes[BarSizeType.BOTTOM],
 };
