c3ed3a70aacdee34d429766a22f672b595863932
hyorish03 <hyorish2001@gmail.com>
2023-05-08 03:10:02 +0900

    엑세스토큰 axios에 넣기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GoogleLogin from 'components/GoogleLogin/GoogleLogin';
-import Login from 'Login/Login';
 import Main from './Main';
 
 function App() {
   return (
     <Routes>
       <Route path="/redirect" element={<GoogleLogin />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Main />} />
     </Routes>
   );
--- src/Login/Login.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Login() {
-  return <div>sdf</div>;
-}
-
-export default Login;
