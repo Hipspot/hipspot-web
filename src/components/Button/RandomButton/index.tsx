86cccd98a8151eb8046db00cfc13266d766c6d3d
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2023-05-15 12:00:39 +0900

    Feat: 랜덤버튼, 내 위치찾기 버튼 위치 이동, 빈 이미지 처리 (#74)
    * Feat: 즐겨찾기 UI 및 상태관리
    
    * Feat: 카페 Type 수정, 즐겨찾기 상태 원격 연동
    
    * 즐겨찾기 버튼 구현
    
    * 랜덤버튼 UI 및 클릭시 토스트메시지
    
    * main과 머지
    
    * Feat: 맵 하단 버튼 위치 조정 #72
    
    * Feat: 이미지 없을 때 예외처리 #72
    
    * 카페정보 API 요청
    
    * TabBarWrapper 스타일 수정
    
    * 카페아이디 랜덤으로 받아오기
    
    * Fix: netlify pnpm install error 수정
    
    ---------
    
    Co-authored-by: sjsjsj1246 <sjsjsj1246@gmail.com>

--- src/components/Button/RandomButton/index.tsx
+++ src/components/Button/RandomButton/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import * as S from './style';
 import { RandomButtonIcon } from '@assets/index';
-import { toast } from 'react-hot-toast';
+import { useRecoilValue } from 'recoil';
+import { tabStateAtom } from '@states/infoWindow';
+import axios from 'axios';
 
 function RandomButton() {
-  const onClick = () => {
-    toast.success('랜덤버튼을 클릭하였습니다.');
+  const { top } = useRecoilValue(tabStateAtom);
+
+  const handleClick = async () => {
+    try {
+      const response = await axios.get(`https://api.hipspot.xyz/map`);
+      const { data } = response;
+      const cafeIds = data && data.map((cafe: any) => cafe.properties.cafeId);
+      const randomArray = cafeIds[Math.floor(Math.random() * cafeIds.length)];
+      const [randomCafeId] = randomArray;
+      console.log(randomCafeId);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <S.Wrapper onClick={onClick}>
+    <S.Wrapper onClick={handleClick} top={top}>
       <RandomButtonIcon />
     </S.Wrapper>
   );
 }
-
 export default RandomButton;
