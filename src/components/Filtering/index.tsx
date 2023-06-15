a684f1caa4954ab73fd7b674c6e674a60d93623f
yeoularu <yeoularu@gmail.com>
2023-06-15 19:13:28 +0900

    feat: 플러터로 온보딩 필터 요청

--- src/components/Filtering/index.tsx
+++ src/components/Filtering/index.tsx
@@ -2,8 +2,15 @@ import styled from '@emotion/styled';
 import filterDataList from '@constants/filterDataList';
 import { FilterId } from '@libs/types/filter';
 import FilterItem from './FilterItem';
+import { useEffect } from 'react';
+import messageToFlutter from '@libs/webview/messageToFlutter';
+import { MessageToFlutterType } from '@constants/flutterCallback';
 
 export default function Filtering() {
+  useEffect(() => {
+    messageToFlutter(MessageToFlutterType.getOnboardingFilter, null);
+  }, []);
+
   return (
     <Wrapper>
       {Object.values(filterDataList).map((filter, index: FilterId) => (
